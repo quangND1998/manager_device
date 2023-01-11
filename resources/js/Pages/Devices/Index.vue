4af8ea48519a52322cacc60ee6bb8dea03e8ef05
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-01-11 16:17:56 +0700

    fix_icon

--- resources/js/Pages/Devices/Index.vue
+++ resources/js/Pages/Devices/Index.vue
@@ -103,7 +103,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(device, index) in devices.data" :key="index"
+          <tr v-for="(device, index) in devices" :key="index"
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"><input
                 type="checkbox" class="checkbox" v-model="selected" :value="device.id"></td>
@@ -138,7 +138,7 @@
                   class="absolute inline-block top-0 right-0 bottom-auto left-auto translate-x-2/4 -translate-y-1/2 rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 py-1 px-2.5 text-xl leading-none text-center whitespace-nowrap align-baseline font-bold bg-gray-400 text-white rounded-full z-10">
                   <i class="fa fa-times" aria-hidden="true" title="Disable Default App" @click="disableDefaultApp(device.id)" ></i></div>
                 <img class="w-15 h-15 rounded-full "
-                  :src="`data:image/png;base64,${device.default_app.icon}`" alt="Rounded avatar">
+                  :src="device.default_app.icon" alt="Rounded avatar">
               </div>
               <div class="text-center pt-2"  v-if="device.default_app" ><strong class="justify-center ">{{ device.default_app.appName }}</strong></div>
             </th>
@@ -152,7 +152,7 @@
         </tbody>
       </table>
     </div>
-    <pagination :links="devices.links" />
+  
   </section>
 
 </template>
@@ -226,7 +226,7 @@ export default {
   },
 
   props: {
-    devices: Object,
+    devices: Array,
     errors: Object,
     // wifis: Array,
     applications: Array
