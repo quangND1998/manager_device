475b90e1855c217e37a5dd4325403d8d3ea3d075
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-01-11 13:56:05 +0700

    main

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -31,7 +31,7 @@
              class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full text-label"
             v-model="form.password"
             required
-          
+            autocomplete="current-password"
           />
           <span class="span-hidden">
             <i
