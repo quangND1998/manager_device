f353350c9d30ba01ed84c3b87e30e9b8325b63b2
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-01-11 16:18:39 +0700

    main

--- app/Models/Applicaion.php
+++ app/Models/Applicaion.php
@@ -10,7 +10,10 @@ class Applicaion extends Model
     use HasFactory;
     protected $table = 'applications';
     protected $fillable = ['id',    'appName',  'packageName','default', 'icon', 'version', 'device_id',  'created_at',  'updated_at'];
-
+    protected $hidden = [
+        'icon',
+        // 'remember_token',
+    ];
     public function device()
     {
         return $this->belongsTo(Devices::class, 'device_id');
