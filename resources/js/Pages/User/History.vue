5e8c6130f8fc23cf53322c11a7f899b69d16378b
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-01-30 11:12:39 +0700

    main

--- resources/js/Pages/User/History.vue
+++ resources/js/Pages/User/History.vue
@@ -1,6 +1,6 @@
 <template>
-   <div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-5">
- 
+    <div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-5">
+
         <table class="w-full text-xl text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xl text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
@@ -19,28 +19,39 @@
                     }}
                     </th>
                     <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{ history.device ? history.device.name:null }}
-                       </th>
-                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"> {{ formatDate(history.time_login) }}</th>
+                        {{ history.device ? history.device.name : null }}
+                    </th>
+                    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"> {{
+                        formatDate(history.time_login)
+                    }}</th>
                     <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"><span
-                            class="text-xl inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">{{ history.ipaddress ? history.ipaddress.country_code :null }}</span>
+                            class="text-xl inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">{{
+                                history.ipaddress ? history.ipaddress.country_code : null
+                            }}</span>
                     </th>
                     <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"><span
-                            class="text-xl inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">{{ history.ipaddress ? history.ipaddress.region_name :null }}</span>
+                            class="text-xl inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">{{
+                                history.ipaddress ? history.ipaddress.region_name : null
+                            }}</span>
                     </th>
                 </tr>
             </tbody>
         </table>
+        <pagination :links="histories.links" />
     </div>
 </template>
 
 <script>
 import Layout from "@/Pages/User/Layout";
+import Pagination from "@/Components/Pagination";
 export default {
     layout: Layout,
     props: {
         user: Object,
         histories: Object
+    },
+    components: {
+        Pagination
     },
 }
 </script>
