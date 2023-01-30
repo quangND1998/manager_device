9cd735d4ba838a0a93bab21aac6c0e10d8811cd8
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-01-30 14:01:00 +0700

    mainư

--- resources/js/Pages/User/MenuUserResponsive.vue
+++ resources/js/Pages/User/MenuUserResponsive.vue
@@ -2,27 +2,6 @@
   <ul class="grid gap-4 grid-cols-3">
     <li class="nav-item">
       <Link
-        :href="route('user.detail.index', user.id)"
-        class="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-white hover:opacity-75"
-      >
-        <icon
-          name="information"
-          class="icon icon-tabler icon-tabler-home"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <span class="ml-2">Information</span>
-      </Link>
-    </li>
-
-    <li class="nav-item">
-      <Link
         :href="route('user.devices.index', user.id)"
         class="px-3 py-2 flex items-center text-xl uppercase font-bold leading-snug text-white hover:opacity-75"
       >
