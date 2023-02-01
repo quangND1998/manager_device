bfa45ccbeb6ffe22497b73ebd26148f2c8f2325b
Nguyen Nga <ngant1096@gmail.com>
2023-02-01 14:46:43 +0700

    edit save paypal

--- app/Http/Controllers/Payment/PricingController.php
+++ app/Http/Controllers/Payment/PricingController.php
@@ -12,17 +12,11 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 use Carbon\Carbon;
-
-use PayPal\Api\ItemList;
-use PayPal\Api\Payer;
-use PayPal\Api\Payment;
-use PayPal\Api\PaymentExecution;
-use PayPal\Api\RedirectUrls;
-use PayPal\Api\Transaction;
-use PayPal\Auth\OAuthTokenCredential;
-use PayPal\Rest\ApiContext;
+use App\Models\Payment;
+use App\Http\Controllers\Traits\PayPalTrait;
 class PricingController extends Controller
 {
+    use PayPalTrait;
     public function index(){
         Session::forget('cart');
         $package_products = ProductPackage::where('state', true)->orderBy('id_priority')->get();
@@ -91,35 +85,24 @@ class PricingController extends Controller
         return Inertia::render('Payment/Response');
     }
     public function response_paypal(Request $request){
-        // return $request;
-        // $payment_id = Session::get('paypal_payment_id');
-        // /** clear the session payment ID **/
-        // Session::forget('paypal_payment_id');
-        // if (empty(Input::get('PayerID')) || empty(Input::get('token'))) {
-        //     Session::put('error', 'Payment failed');
-        //     return Redirect::to('/');
-        // }
-        // $payment = Payment::get($payment_id, $this->_api_context);
-        // $execution = new PaymentExecution();
-        // $execution->setPayerId($request->PayerID);
-        // dd($execution);
-
-        if($request->PayerID != null){
+        $result = $this->responsePayPal($request);
+        if ($result->getState() == 'approved') {
             $cart = $request->session()->get('cart');
             $item = $cart->items;
             $transaction = $request->paymentId;
-            $this->saveSuccessdataPaypal($request);
+            $this->saveSuccessdataPaypal($request,$result);
             // dd($cart);
             return Inertia::render('Payment/Response_paypal',compact('cart','item','transaction'))->with('success', 'Transaction payment success!');
         }else{
             return Inertia::render('Payment/Response_paypal')->with('danger', 'Payment failed.');
         }
     }
-    public function saveSuccessdataPaypal($data){
+    public function saveSuccessdataPaypal($data,$result){
 
         if (Session::has('cart')) {
             $cart = Session::get('cart');
             $timelimit = Carbon::now()->addDays($cart->items['item']->package_time);
+
             $des = $cart->items['item']->name . ', Number device : ' .$cart->items['number_device'];
             $user =  User::findOrFail(Auth::user()->id);
             $user->number_device = $cart->items['number_device'];
@@ -127,7 +110,7 @@ class PricingController extends Controller
             $user->save();
             $role = Role::where('name','Pro')->first();
             $user->roles()->sync($role);
-            $this->savePaymentPayPal($data,$des,$cart->items);
+            $this->savePaymentPayPal($result,$des,$cart->items);
             // save bill
             Session::forget('cart');
         }
@@ -135,18 +118,18 @@ class PricingController extends Controller
     }
     public function savePaymentPayPal($data,$des,$items)
     {
-        // dd($items['price']);
-        // dd($data['vpc_Amount'] / 100);
-        $check = payment::where('merch_TxnRef', $data->paymentId)->count();
+        // dd($data->payer);
+        // dd($data);
+        $check = Payment::where('merch_TxnRef', $data->id)->count();
         if($check == 0){
             $payment = new Payment;
             $payment->pay_gate = "paypal";
-            $payment->pay_type = 'web';
-            $payment->amount = $items['price'];
-            $payment->transID = $data->paymentId;
-            $payment->merch_TxnRef = $data->paymentId;
-            $payment->card_number = "";
-            $payment->state = "Approved";
+            $payment->pay_type = $data->payer->payment_method;
+            $payment->amount =  $data->transactions[0]->amount->total;
+            $payment->transID = $data->id;
+            $payment->merch_TxnRef = $data->id;
+            $payment->card_number = $data->cart;
+            $payment->state = $data->state;
             $payment->description = $des;
             $payment->user_id = Auth::user()->id;
             $payment->save();
