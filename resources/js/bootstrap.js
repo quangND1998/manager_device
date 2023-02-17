96dbafe4d8120e2d535908a431b919b9cac6dcfe
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-02-17 08:24:16 +0700

    main

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -66,7 +66,7 @@ var connectionOptions = {
     "force new connection": true,
     "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
     "timeout": 10000, //before connect_error and connect_timeout are emitted.
-    "transports": ["websocket"]
+    "transports": ["websocket", "wss"]
 };
 console.log(`${window.location.protocol}//${window.location.hostname}:3000`)
 window.socketio = io("http://178.128.125.34:3000", connectionOptions);
