e04d2baa381e104ecb5ab0ad2140259c1dd72f46
quangND1998 <quangnd620@wru.vn>
2023-02-18 12:49:47 +0700

    main

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -69,4 +69,4 @@ var connectionOptions = {
     "transports": ["websocket"]
 };
 // console.log(`${window.location.protocol}//${window.location.hostname}:3000`)
-window.socketio = io("http://devstartup.holomia.com:3000", connectionOptions);
+window.socketio = io("http://startup.holomia.com:3001", connectionOptions);
