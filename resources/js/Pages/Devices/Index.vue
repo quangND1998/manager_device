4bf3ec7517e27ddaac0e4bbd35ec6a7c76c6ef1f
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-02-20 17:32:19 +0700

    main

--- resources/js/Pages/Devices/Index.vue
+++ resources/js/Pages/Devices/Index.vue
@@ -92,7 +92,7 @@
 
       </div>
     </div>
-    <button type="button" @click="FreshDevice()" class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-2xl leading-tight  rounded-full shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out"><i class="fa fa-refresh mr-2" aria-hidden="true"></i>Fresh Active Device</button>
+    <button type="button" @click="FreshDevice()" class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-2xl leading-tight  rounded-full shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out"><i class="fa fa-refresh mr-2" aria-hidden="true"></i>Active devices</button>
    
     <div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-5">
 
