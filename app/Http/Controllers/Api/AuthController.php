17d0bdec282a7628b6cedf9fbfe3d65ba08cd3f3
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-06-01 10:53:06 +0700

    api-laucher

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -59,10 +59,10 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->validate(['token' => 'required']);
-
+        // $request->validate(['token' => 'required']);
+        $token= request()->bearerToken();
         try {
-            JWTAuth::invalidate($request->input('token'));
+            JWTAuth::invalidate($token);
             return response()->json('You have successfully logged out.', Response::HTTP_OK);
         } catch (JWTException $e) {
             return response()->json('Failed to logout, please try again.', Response::HTTP_BAD_REQUEST);
