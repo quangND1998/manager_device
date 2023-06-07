0c6d3ecc25237fbc8838b7cd8490b3a094c4658b
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-06-07 17:57:29 +0700

    api-laucher

--- app/Events/LaunchAppEvent.php
+++ app/Events/LaunchAppEvent.php
@@ -10,7 +10,8 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\File;
-class LaunchAppEvent implements ShouldBroadcast
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+class LaunchAppEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $device;
