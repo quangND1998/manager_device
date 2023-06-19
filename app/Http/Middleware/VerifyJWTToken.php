a6993a508e2287d8f6eeb9e9c929f9fe64a9d4d3
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-06-19 09:10:13 +0700

    api-laucher

--- app/Http/Middleware/VerifyJWTToken.php
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -33,7 +33,7 @@ class VerifyJWTToken
                 return response()->json(['status' => 'Token is Invalid']);
             } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
 
-                return response()->json(['status' => 'Token is Expired'],401);
+                return response()->json(['status' => 'Token is Expired'], Response::HTTP_UNAUTHORIZED);
             } else {
 
                 return response()->json(['status' => 'Authorization Token not found'],  Response::HTTP_BAD_REQUEST);
