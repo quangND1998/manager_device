8eb2446a4f832256eca35ed7338527f03479f8e4
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-07-17 14:54:51 +0700

    serve

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,10 @@ import './bootstrap';
 import Vue from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue'
 // import { InertiaProgress } from '@inertiajs/progress'
-import VueCompositionAPI from '@vue/composition-api'
+
 import moment from 'moment';
 // InertiaProgress.init({})
-Vue.use(VueCompositionAPI)
+
 createInertiaApp({
     resolve: name =>
         import (`./Pages/${name}`),
