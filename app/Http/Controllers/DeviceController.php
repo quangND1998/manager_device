1ed9e98e7cd80f9701068bab374ee0ecb98f7853
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-07-18 16:04:29 +0700

    api-laucher

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -44,7 +44,7 @@ class DeviceController extends Controller
     }
     public function index(Request $request)
     {
-
+        // dd(Carbon::now()->addMinutes(0)->addSeconds(30));
         $user = Auth::user();
         $sortBy = $request->sortBy ? $request->sortBy : 'id';
         $sort_Direction = $request->sortDirection ?  $request->sortDirection : 'asc';
