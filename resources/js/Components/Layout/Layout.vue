ff2da4725ebd8505cecb0eeba1d1c9ef76294d80
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-07-20 08:44:04 +0700

    merge_code

--- resources/js/Components/Layout/Layout.vue
+++ resources/js/Components/Layout/Layout.vue
@@ -48,13 +48,18 @@ export default {
     listenNotificationGroup() {
       this.sockets.subscribe(`time-play-notification.${this.$page.props.auth.user.id}:App\\Events\\TimeEndGroupNotification`, (data) => {
         console.log(data)
-        this.$toast.warning(`Group ${data.group_name} Timer Ends`, {
-          // override the global option
-          position: 'bottom-right',
+        // this.$toast.warning(`Group ${data.group_name} Timer Ends`, {
+        //   // override the global option
+        //   position: 'bottom-right',
 
-          duration: 60000,
-          dismissible: true
-        })
+        //   duration: 60000,
+        //   dismissible: true
+        // })
+
+        this.$swal(`Group ${data.group_name} Timer Ends`, {
+          icon: "warning",
+          timer: 60000,
+        });
       });
     },
     listenNotificationDevice() {
@@ -62,13 +67,10 @@ export default {
       if (this.user) {
         this.sockets.subscribe(`time-end-device.${this.$page.props.auth.user.id}:App\\Events\\TimeEndDeviceNotification`, (data) => {
           console.log(data)
-          this.$toast.warning(`Device ${data.device_name} Timer Ends`, {
-            // override the global option
-            position: 'bottom-right',
-
-            duration: 60000,
-            dismissible: true
-          })
+          this.$swal(`Group ${data.group_name} Timer Ends`, {
+            icon: "warning",
+            timer: 60000,
+          });
         });
       }
 
