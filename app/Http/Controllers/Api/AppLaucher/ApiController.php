554b5bb49126034af8681e04b399fdc5ce2b7903
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-07-20 09:49:17 +0700

    api-laucher

--- app/Http/Controllers/Api/AppLaucher/ApiController.php
+++ app/Http/Controllers/Api/AppLaucher/ApiController.php
@@ -213,14 +213,16 @@ class ApiController extends Controller
     {
         // return !$user->hasPermissionTo('user-manager') ?
 
-        $user = User::with(['devices.last_login.ipaddress', 'groups', 'app_windows'])->withCount('app_windows')->withCount('devices')->withCount('groups')->find(Auth::user()->id);
+        $user = User::with(['devices.last_login.ipaddress', 'app_windows','groups' =>function($q){
+            $q->withCount('devices');
+        }])->withCount('app_windows')->withCount('devices')->withCount('groups')->find(Auth::user()->id);
        
         $response  = [
             "app_windows_count" => $user->hasPermissionTo('user-manager') ? AppWindow::count() : $user->app_windows_count,
             "devices_count" => $user->hasPermissionTo('user-manager') ? Devices::count() : $user->devices_count,
             "groups_count" => $user->hasPermissionTo('user-manager') ? Groups::count() : $user->groups_count,
             "devices" =>  $user->hasPermissionTo('user-manager') ? Devices::get() : $user->devices,
-            "groups" =>  $user->hasPermissionTo('user-manager') ? Groups::get() : $user->groups,
+            "groups" =>  $user->hasPermissionTo('user-manager') ? Groups::withCount('devices')->get() : $user->groups,
             "app_windows" =>  $user->hasPermissionTo('user-manager') ? AppWindow::get() : $user->app_windows,
             // "device_locations" => $user->hasPermissionTo('user-manager')? LocationResource::collection(Devices::with('last_login.ipaddress')->get()): LocationResource::collection($user->devices)
 
