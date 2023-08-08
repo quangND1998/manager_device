2887aa6a94c472705c2126dc1a41a41da6bb21d2
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-08-08 10:05:25 +0700

    serve

--- resources/js/Pages/Admin/UpdateUser.vue
+++ resources/js/Pages/Admin/UpdateUser.vue
@@ -72,7 +72,7 @@
                 <button
                   @click.prevent="save()"
                   type="submit"
-                  class="inline-block px-6 py-2.5 bg-blue-600 text-white font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-lg transition duration-150 ease-in-out
+                  class="inline-block px-6 py-2.5 bg-blue-600 text-white font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-lg transition duration-150 ease-in-out"
                 >Import</button>
               </div>
             </form>
