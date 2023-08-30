ee77780fe1e67dc717442389030d2993d43bb532
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2023-08-30 10:17:09 +0700

    serve

--- resources/js/Pages/Window/Index.vue
+++ resources/js/Pages/Window/Index.vue
@@ -1,27 +1,17 @@
 <template>
   <section class="content">
-    <ContentHeaderVue class="pl-0" :name="'Window App'" />
+    <ContentHeaderVue class="pl-0" :name="'Free roam VR'" />
     <alert :dismissible="true"></alert>
     <div class="my-5">
-      <button
-          type="button"
-          class="inline-block px-8 py-4 bg-blue-600 text-white font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          data-toggle="modal"
-          data-target="#exampleModal"
-          @click="clickModal()"
-        >Upload App</button>
+      <button type="button"
+        class="inline-block px-8 py-4 bg-blue-600 text-white font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        data-toggle="modal" data-target="#exampleModal" @click="clickModal()">Upload App</button>
     </div>
-    
+
 
     <!-- Modal -->
-    <div
-      class="modal fade"
-      id="exampleModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -31,68 +21,39 @@
           </div>
           <div class="modal-body">
             <form @submit.prevent="save">
-              <div
-                class="w-1/4 bg-gray-200 rounded-full dark:bg-gray-700 mb-8 ml-3"
-                v-if="form.progress"
-              >
-                <div
-                  class="bg-blue-600 text-xl font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-                  :style="{ width: form.progress.percentage + '%' }"
-                >{{ form.progress.percentage }}%</div>
+              <div class="w-1/4 bg-gray-200 rounded-full dark:bg-gray-700 mb-8 ml-3" v-if="form.progress">
+                <div class="bg-blue-600 text-xl font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                  :style="{ width: form.progress.percentage + '%' }">{{ form.progress.percentage }}%</div>
               </div>
 
               <div>
-                  <label for="company" class="block mb-2 text-xl font-medium text-gray-900 text-left">App URL</label>
-                  <input rows="4" type="file" @change="onFileChange" accept=".apk"
-                    class="block p-2.5 w-full text-xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                    placeholder="R:\Project\HolomiaVRZ_230403b_MetaQuest.apk" />
-                </div>
+                <label for="company" class="block mb-2 text-xl font-medium text-gray-900 text-left">App URL</label>
+                <input rows="4" type="file" @change="onFileChange" accept=".apk"
+                  class="block p-2.5 w-full text-xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="R:\Project\HolomiaVRZ_230403b_MetaQuest.apk" />
+              </div>
               <div v-if="form.packageName" class="form-group" :class="errors.name ? 'is-valid' : ''">
                 <label for="recipient-name" class="col-form-label">Name:</label>
-                <input
-                  type="text"
-                  class="form-control text-xl"
-                  :class="errors.name ? 'is-valid' : ''"
-                  v-model="form.name"
-                  id="recipient-name"
-                  readonly
-                />
+                <input type="text" class="form-control text-xl" :class="errors.name ? 'is-valid' : ''" v-model="form.name"
+                  id="recipient-name" readonly />
                 <div class="text-red-500" v-if="errors.name">{{ errors.name }}</div>
               </div>
               <div class="form-group my-3" :class="errors.path ? 'is-valid' : ''">
                 <label for="recipient-name" class="col-form-label">Path to .exe file :</label>
-                <textarea
-                  type="text"
-                  class="form-control text-xl border-gray-300"
-                  :class="errors.path ? 'is-valid' : ''"
-                  v-model="form.path"
-                  id="recipient-name"
-                ></textarea>
+                <textarea type="text" class="form-control text-xl border-gray-300" :class="errors.path ? 'is-valid' : ''"
+                  v-model="form.path" id="recipient-name"></textarea>
                 <div class="text-red-500" v-if="errors.path">{{ errors.path }}</div>
               </div>
               <div v-if="form.packageName" class="form-group" :class="errors.packageName ? 'is-valid' : ''">
                 <label for="recipient-name" class="col-form-label">Package Name:</label>
-                <input
-                  type="text"
-                  class="form-control text-xl"
-                  :class="errors.packageName ? 'is-valid' : ''"
-                  v-model="form.packageName"
-                  id="recipient-name"
-                  placeholder="com.holomia.holomia"
-                  readonly
-                />
+                <input type="text" class="form-control text-xl" :class="errors.packageName ? 'is-valid' : ''"
+                  v-model="form.packageName" id="recipient-name" placeholder="com.holomia.holomia" readonly />
                 <div class="text-red-500" v-if="errors.packageName">{{ errors.packageName }}</div>
               </div>
-              <div  v-if="form.packageName" class="form-group" :class="errors.version ? 'is-valid' : ''">
+              <div v-if="form.packageName" class="form-group" :class="errors.version ? 'is-valid' : ''">
                 <label for="recipient-name" class="col-form-label">version:</label>
-                <input
-                  type="text"
-                  class="form-control text-xl"
-                  :class="errors.version ? 'is-valid' : ''"
-                  v-model="form.version"
-                  id="recipient-name"
-                  readonly
-                />
+                <input type="text" class="form-control text-xl" :class="errors.version ? 'is-valid' : ''"
+                  v-model="form.version" id="recipient-name" readonly />
                 <div class="text-red-500" v-if="errors.version">{{ errors.version }}</div>
               </div>
               <div class="form-group" :class="errors.path ? 'is-valid' : ''">
@@ -101,24 +62,16 @@
                   icon app
                 </label>
                 <div class="flex items-center justify-center w-full">
-                  <label
-                    class="flex flex-col w-full h-48 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300"
-                  >
+                  <label class="flex flex-col w-full h-48 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
                     <div class="flex flex-col items-center justify-center pt-7">
                       <i class="fa fa-file-o w-12 h-15" aria-hidden="true"></i>
-                      <p
-                        class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600"
-                      >Drag file .jpg .jepg .png</p>
+                      <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">Drag file .jpg .jepg
+                        .png</p>
 
                       <img :src="image" width="50px" />
-                      <input
-                        type="file"
-                        @input="form.icon = $event.target.files[0]"
-                        :class="errors.icon ? 'border-red-500' : ''"
-                        class="opacity-0"
-                        accept="image/*"
-                        @change="onChangeFile"
-                      />
+                      <input type="file" @input="form.icon = $event.target.files[0]"
+                        :class="errors.icon ? 'border-red-500' : ''" class="opacity-0" accept="image/*"
+                        @change="onChangeFile" />
                     </div>
                   </label>
 
@@ -126,16 +79,11 @@
                 </div>
               </div>
               <div class="modal-footer">
-                <button
-                  type="button"
+                <button type="button"
                   class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out mx-2"
-                  data-dismiss="modal"
-                >Cancel</button>
-                <button
-                  @click.prevent="save()"
-                  type="submit"
-                  class="inline-block px-6 py-2.5 bg-blue-600 text-white font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-lg transition duration-150 ease-in-out"
-                >Upload</button>
+                  data-dismiss="modal">Cancel</button>
+                <button @click.prevent="save()" type="submit"
+                  class="inline-block px-6 py-2.5 bg-blue-600 text-white font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-lg transition duration-150 ease-in-out">Upload</button>
               </div>
             </form>
           </div>
@@ -154,76 +102,43 @@
             <th scope="col" class="py-3 px-6 text-xl uppercase ">Icon</th>
             <th scope="col" class="py-3 px-6 text-xl uppercase ">PackageName</th>
             <th scope="col" class="py-3 px-6 text-xl uppercase ">Version</th>
-            <th scope="col" class="py-3 px-6 text-xl uppercase "  v-if="hasAnyPermission(['user-manager'])">User</th>
+            <th scope="col" class="py-3 px-6 text-xl uppercase " v-if="hasAnyPermission(['user-manager'])">User</th>
             <th scope="col" class="py-3 px-6 text-xl uppercase ">
               <span class="sr-only">Edit</span>
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="(app, index) in window_apps.data"
-            :key="index"
-            class="bg-white border-b "
-          >
+          <tr v-for="(app, index) in window_apps.data" :key="index" class="bg-white border-b ">
             <!-- <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"><input
             type="checkbox" class="checkbox" v-model="selected" :value="device.id"></td>-->
-            <th
-              scope="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-            >
+            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
               {{
-              index +1
+                index + 1
               }}
             </th>
-            <th
-              apk="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-            >{{ app.name }}</th>
-            <th
-              apk="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-            >{{ app.path }}</th>
-            <th
-              scope="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-            >
+            <th apk="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">{{ app.name }}</th>
+            <th apk="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">{{ app.path }}</th>
+            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
               <img :src="app.icon" width="50px" />
             </th>
-            <th
-              apk="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-            >{{ app.packageName }}</th>
-            <th
-              apk="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-            >{{ app.version }}</th>
-            <th
-              scope="row"
-              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-              v-if="hasAnyPermission(['user-manager'])"
-            >
+            <th apk="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">{{ app.packageName }}</th>
+            <th apk="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">{{ app.version }}</th>
+            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
+              v-if="hasAnyPermission(['user-manager'])">
               <Link v-if="app.user" :href="route('user.detail.devices', app.user.id)">
-                {{ app.user ?
+              {{ app.user ?
                 app.user.name : null }}
               </Link>
             </th>
             <td class="py-4 px-6 text-right">
-              <button
-                @click="edit(app)"
-                type="button"
-                data-toggle="modal"
-                data-target="#exampleModal"
-                class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out mx-2" 
-              >
+              <button @click="edit(app)" type="button" data-toggle="modal" data-target="#exampleModal"
+                class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out mx-2">
                 Edit
                 Path
               </button>
-              <button
-                type="button"
-                @click="Delete(app.id)"
-                class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-red-400 hover:text-white hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out "
-              >Delete</button>
+              <button type="button" @click="Delete(app.id)"
+                class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-black text-xl leading-tight uppercase rounded shadow-md hover:bg-red-400 hover:text-white hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out ">Delete</button>
             </td>
           </tr>
         </tbody>
@@ -261,9 +176,9 @@ export default {
         id: null,
         name: null,
         path: null,
-        version:null,
+        version: null,
         icon: null,
-        packageName:null
+        packageName: null
       })
     };
   },
@@ -320,7 +235,7 @@ export default {
         .catch((err) => {
           console.log("err ----> ", err);
         });
-      },
+    },
     clickModal() {
       this.editMode = false;
       this.form.reset();
@@ -339,8 +254,8 @@ export default {
         name: null,
         path: null,
         icon: null,
-        version:null,
-        packageName:null
+        version: null,
+        packageName: null
       });
     },
     Delete(id) {
@@ -385,8 +300,8 @@ export default {
       cursor: pointer;
     }
 
-    .fm-item-icon > i,
-    .fm-item-icon > figure > i {
+    .fm-item-icon>i,
+    .fm-item-icon>figure>i {
       color: #6d757d;
     }
 
